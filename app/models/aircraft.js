2ec54ef67acf0afd4c0aa553435f97bb01a2e878
Sukirtharan Rajadurai <sukirpro@gmail.com>
2022-05-08 21:32:46 +0100

    Init

---
+++ app/models/aircraft.js
@@ -0,0 +1,32 @@
+const imagePath = "aircrafts"
+
+module.exports = (sequelize, Sequelize) => {
+    const Aircraft = sequelize.define('aircrafts', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        aircraft_number: {
+            type: Sequelize.STRING,
+        },
+        aircraft_number: {
+            type: Sequelize.STRING,
+            primaryKey: true,
+        },
+        passenger_capacity: {
+            type: Sequelize.STRING,
+        },
+        crew_capacity: {
+            type: Sequelize.STRING
+        },  
+    },{
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: 'deleted_at',      
+        paranoid: true
+    });
+
+    return Aircraft;
+}   
---
+++ app/models/passenger.js
@@ -0,0 +1,48 @@
+const imagePath = "passengers"
+
+module.exports = (sequelize, Sequelize) => {
+    const Passenger = sequelize.define('passengers', {
+        passport_number: {
+            type: Sequelize.STRING,
+            primaryKey: true,
+        },
+        flight_number: {
+            type: Sequelize.STRING,
+        },
+        threat_level: {
+            type: Sequelize.STRING,
+        },
+        forename: {
+            type: Sequelize.STRING
+        },
+        family_name: {
+            type: Sequelize.STRING
+        },
+        gender: {
+            type: Sequelize.STRING
+        },
+        dob: {
+            type: Sequelize.DATE,
+        },
+        nationality: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        revenue: {
+            type: Sequelize.STRING,
+            // allowNull: false
+        },
+        seat_number: {
+            type: Sequelize.STRING
+        },
+    }, {
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: 'deleted_at',
+
+        paranoid: true
+    });
+
+    return Passenger;
+}   
